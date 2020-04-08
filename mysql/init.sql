85b6bc37d34cd76ea06b3be70dfe76bb6b8bb664
fnaoto <f.naoto832+github@gmail.com>
2020-04-08 18:35:03 +0900

    Fix test db column.

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,8 +5,6 @@ CREATE TABLE IF not exists `from_db`.`from_table`
 (
  `id`               INT(20) AUTO_INCREMENT,
  `name`             VARCHAR(20) NOT NULL,
- `created_at`       Datetime DEFAULT NULL,
- `updated_at`       Datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
