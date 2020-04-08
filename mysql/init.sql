a2e5d339122eb0b2d978a6c8ea018528410a6a41
fnaoto <f.naoto832+github@gmail.com>
2020-04-08 18:18:36 +0900

    Add to create init data.

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,4 +1,7 @@
-create table IF not exists `from_table`
+CREATE DATABASE IF not exists `from_db`;
+CREATE DATABASE IF not exists `to_db`;
+
+CREATE TABLE IF not exists `from_db`.`from_table`
 (
  `id`               INT(20) AUTO_INCREMENT,
  `name`             VARCHAR(20) NOT NULL,
@@ -6,3 +9,5 @@ create table IF not exists `from_table`
  `updated_at`       Datetime DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT `from_db`.`from_table` (`name`) VALUES ('test');
