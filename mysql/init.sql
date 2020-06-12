c7ce5a05e4c0d653a1d616b86b020611cef3c680
fnaoto <f.naoto832+github@gmail.com>
2020-06-12 10:43:39 +0900

    Add merge test for duplicate index.

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,6 @@
+-- FROM
+
 CREATE DATABASE IF not exists `from_db`;
-CREATE DATABASE IF not exists `to_db`;
 
 CREATE TABLE IF not exists `from_db`.`from_table`
 (
@@ -8,4 +9,23 @@ CREATE TABLE IF not exists `from_db`.`from_table`
     PRIMARY KEY (`id`)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT `from_db`.`from_table` (`name`) VALUES ('test');
+INSERT `from_db`.`from_table` (`name`) VALUES ('test1'); -- duplicate
+INSERT `from_db`.`from_table` (`name`) VALUES ('test2');
+INSERT `from_db`.`from_table` (`name`) VALUES ('test3');
+CREATE UNIQUE INDEX name_index on from_table(name);
+
+-- TO
+
+CREATE DATABASE IF not exists `to_db`;
+
+CREATE TABLE IF not exists `to_db`.`to_table`
+(
+ `id`               INT(20) AUTO_INCREMENT,
+ `name`             VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+INSERT `from_db`.`from_table` (`name`) VALUES ('test4');
+INSERT `from_db`.`from_table` (`name`) VALUES ('test5');
+INSERT `from_db`.`from_table` (`name`) VALUES ('test1'); -- duplicate
+CREATE UNIQUE INDEX name_index on from_table(name);
